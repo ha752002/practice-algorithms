72552a0f7ab55dbf69530610d9f2386d03342f4e
ha752002 <honggha75@gmail.com>
2023-12-22 16:56:33 +0700

    practive algorithms

---
+++ ex-algorithms/ex-1/a-very-big-sum.ts
@@ -0,0 +1,16 @@
+
+/*
+ * Complete the 'aVeryBigSum' function below.
+ *
+ * The function is expected to return a LONG_INTEGER.
+ * The function accepts LONG_INTEGER_ARRAY ar as parameter.
+ */
+
+function aVeryBigSum(ar: number[]): number {
+    let sum = 0;
+    for(let num of ar){
+        sum += num;
+    }
+    return sum;
+
+}
